5f43113edea023acd01b75afa6c5afe7a9d35568
phil <philip.waller@gmail.com>
2017-08-20 16:25:54 +1000

    2.10_Supervisor: Commenting behaviour.

--- src2/super.erl
+++ src2/super.erl
@@ -7,6 +7,8 @@
 
 %%------------------------------------------------------------------------------
 %% 
+%% # Using the System
+%%
 %% 1> register(echo,spawn(echo,listener,[])).
 %% 2> Pid=spawn(talk,worker,[]).
 %%
@@ -19,9 +21,33 @@
 %%      . kills the echo server
 %%      . all current workers fail with `error:badarg`.
 %%
-
-%%------------------------------------------------------------------------------
 %%
+%% # Adding a supervisor
+%%
+%% * Kill 'echo' server:
+%%      . new 'echo' server spawned immediately.
+%%      . no break in 'count' of 'talk' client.
+%%
+%% * Kill 'talk' client:
+%%      . new 'talk' client spawned immediately.
+%%      . 'count' reset to zero.
+%%
+%% # Add delay to Respawn of 'echo' server:
+%%
+%% * Kill 'echo' server:
+%%      . current 'talk' client dies with `error:badarg` as it tries to 
+%%        communicate with killed 'echo' server
+%%      . new 'echo' server spawned.
+%%      . new 'talk' server spawned.
+%%      . 'count' reset to zero.
+%%
+%%  * Kill 'talk' client:
+%%      . As above
+%%      . new 'talk' with 'count' reset.
+%%
+%%
+%%------------------------------------------------------------------------------
+%%      . supervisor . 
 %%
 
 super() ->
@@ -48,9 +74,4 @@ loop(E,T) ->
             io:format("echo re-spawned.~n"),
             loop(NewE,T)
     end.
-
-
-
-
-    
 
