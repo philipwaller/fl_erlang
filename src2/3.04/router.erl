454900c94b12171514ccd6f6c661f7a1d1bec121
phil <philip.waller@gmail.com>
2017-08-24 22:47:36 +1000

    3.04 Submitted.

--- src2/3.04/router.erl
+++ src2/3.04/router.erl
@@ -11,36 +11,37 @@ start() ->
 
 
 %%------------------------------------------------------------------------------
-%%
+%% Init
 %%
 
 init() ->
 	process_flag(trap_exit, true),
 	?MODULE:loop(workers(frequency_config())).
 
+% Worker configurations:
 frequency_config() -> [
 		{freqA, [10,11,12,13,14,15]},
 		{freqB, [20,21,22,23,24,25]}
 	].
 
+% Create Workers:
 workers(Cfg) -> 
 	[?MODULE:worker(Name,Fs) || {Name,Fs} <- Cfg].
 
+% Create worker: tuple {registered name, pid, list frequencies}
 worker(Nid,Fs) ->
 	Pid = spawn_link(frequency,init,[Nid,Fs]),
         register(Nid,Pid),
 	{Nid,Pid,Fs}.
 
 %%------------------------------------------------------------------------------
-%%
+%% Main
 %%
 
 loop(Wrks) ->
-	io:format("loop(~w)~n",[Wrks]),
 	receive
 		{request,_Pid,allocate} = Request ->
-                        {Nid,_Wid,_Fs} = load_bal(Wrks),
-                        Nid ! Request,
+			process_allocation(Wrks,Request),
 			?MODULE:loop(Wrks);
 
 		{request,_Pid,{deallocate,_F}} = Request -> 
@@ -59,6 +60,18 @@ loop(Wrks) ->
 			?MODULE:loop(Wrks)
 	end.
 
+% Allocate Frequency: handed off to worker
+process_allocation(Wrks,Request),
+	{Nid,_Wid,_Fs} = load_bal(Wrks),
+	Nid ! Request.
+
+% Load Balancer: choose worker to handle request
+load_bal(Wrks) ->
+	% Random load balancing
+        lists:nth(rand:uniform(length(Wrks)),Wrks).
+
+
+% Deallocate Frequency: identify worker handling frequency value.
 process_deallocation(Wrks,{request,Pid,{deallocate,F}}=Request) ->
         try frequency_worker(F,Wrks) of
                 {Nid,_Pid,_Fs} -> Nid ! Request
@@ -66,19 +79,16 @@ process_deallocation(Wrks,{request,Pid,{deallocate,F}}=Request) ->
                 _:_ -> Pid ! {reply,{illegal_deallocation_request,F}}
         end.
 
+% Frequency Worker: worker with frequency
+frequency_worker(F,Wrks) ->
+        hd([{Nid,Pid,Fs} || {Nid,Pid,Fs} <- Wrks, lists:member(F,Fs)]).
+
+% Respawn dead worker.
 process_exit(Wrks,{'EXIT',Pid,_Reason}) ->
         case lists:keytake(Pid,2,Wrks) of
                 {value,{Nid,_Wid,Fs},NewWrks} -> [worker(Nid,Fs)|NewWrks];
                 _ -> Wrks
         end.
-
-% Random load balancing
-load_bal(Wrks) ->
-        lists:nth(rand:uniform(length(Wrks)),Wrks).
-
-% Frequency Worker
-frequency_worker(F,Wrks) ->
-        hd([{Nid,Pid,Fs} || {Nid,Pid,Fs} <- Wrks, lists:member(F,Fs)]).
 
 %%------------------------------------------------------------------------------
 %% 	. Functional Interface .
